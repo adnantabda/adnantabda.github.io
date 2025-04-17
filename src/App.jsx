e67dab6f5081dcede880881f77be1f34961f27dc
Adnan <adnantabda@gmail.com>
2025-04-17 23:40:44 +0300

    refactor: component structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Main from './components/Main';
+import WhatIOffer from './components/Offer'
+import Skills from './components/Skills';
+import ProjectsSection from './components/Projects';
+import BlogSection from './components/Blog';
+import ContactSection from './components/Contact';
+import Footer from './components/Footer';
 // import Hero from './components/Hero';
 // import About from './components/About';
 // import Projects from './components/Projects';
@@ -15,6 +20,12 @@ function App() {
       <Header />
       <Hero></Hero>
       <Main />
+      <WhatIOffer />
+      <Skills />    
+      <ProjectsSection />
+      <BlogSection />
+      <ContactSection />
+      <Footer />
 
     </>
   );
