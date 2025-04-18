ccb861f2175aa27f698aa884b533ea4e3647d79f
Adnan <adnantabda@gmail.com>
2025-04-18 23:20:56 +0300

    Remove:  unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,6 @@ import ProjectsSection from './components/Projects';
 import BlogSection from './components/Blog';
 import ContactSection from './components/Contact';
 import Footer from './components/Footer';
-// import Hero from './components/Hero';
-// import About from './components/About';
-// import Projects from './components/Projects';
-// import Skills from './components/Skills';
-// import Contact from './components/Contact';
-
 
 function App() {
   return (
