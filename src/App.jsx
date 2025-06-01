08e8fb4211144f31a701780576847158f9d0cb41
Adnan <adnantabda@gmail.com>
2025-06-01 13:16:14 +0300

    Refactor App layout and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Main from './components/Main';
-import WhatIOffer from './components/Offer'
 import Skills from './components/Skills';
 import ProjectsSection from './components/Projects';
-import BlogSection from './components/Blog';
 import ContactSection from './components/Contact';
 import Footer from './components/Footer';
+import AboutMe from './components/Main';
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-slate-950">
       <Header />
-      <Hero></Hero>
-      <Main />
-      <WhatIOffer />
-      <Skills />    
+      <Hero />
+      <AboutMe />
+      <Skills />
       <ProjectsSection />
-      <BlogSection />
       <ContactSection />
       <Footer />
-
-    </>
+    </div>
   );
 }
 
